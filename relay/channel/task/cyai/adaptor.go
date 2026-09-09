57d0d2eef797a3fc9c0ca7c33aee8bd1b5f2f668
dong <1593865308@qq.com>
2026-09-09 14:34:51 +0800

    fix(pricing): align remaining video resolution multipliers to Seedance defaults (1080p=2.49, 4k=5.08)

--- relay/channel/task/cyai/adaptor.go
+++ relay/channel/task/cyai/adaptor.go
@@ -30,8 +30,8 @@ func (a *TaskAdaptor) GetModelList() []string { return ModelList }
 var defaultResolutionRatio = map[string]float64{
 	"480p":  1.0,
 	"720p":  1.0,
-	"1080p": 1.25,
-	"4k":    0.32,
+	"1080p": 2.49,
+	"4k":    5.08,
 }
 
 func (a *TaskAdaptor) EstimateBilling(c *gin.Context, info *relaycommon.RelayInfo) map[string]float64 {
--- web/default/src/features/system-settings/billing/index.tsx
+++ web/default/src/features/system-settings/billing/index.tsx
@@ -52,7 +52,7 @@ const defaultBillingSettings: BillingSettings = {
   'billing_setting.billing_expr': '{}',
   'tool_price_setting.prices': '{}',
   'video_pricing_setting.resolution_ratio':
-    '{"480p":1,"720p":1,"1080p":1.25,"4k":0.32}',
+    '{"480p":1,"720p":1,"1080p":2.49,"4k":5.08}',
   'video_pricing_setting.resolution_ratio_by_model': '{}',
   TopupGroupRatio: '',
   GroupRatio: '',
